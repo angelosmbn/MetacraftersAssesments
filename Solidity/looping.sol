5b00412133ab0ece4b823d4843deb03a54bc6b38
Limits <131283311+angelosmbn@users.noreply.github.com>
2024-09-29 21:17:17 +0800

    Update looping.sol

--- Solidity/looping.sol
+++ Solidity/looping.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity *0.8.26;
 
+//error will occur due to insufficient gas or it exceeded the gas limit.
 contract Looping {
     uint public num;
     function loops() external  {
